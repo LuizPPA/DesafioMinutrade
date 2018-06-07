07b60277bb3cc8cd5949876805a3e04cc24b2eb8
Luiz Philippe <luiz@phari.solutions>
2018-06-07 19:49:43 -0300

    Snacks creation added

---
+++ desafio-minutrade-frontend/src/app/snack-list/create-snack/create-snack.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormControl } from '@angular/forms'
+import { SnackService } from '../../shared/snack.service'
+import Snack from '../../snack/snack.model'
+
+@Component({
+  selector: 'app-create-snack',
+  templateUrl: './create-snack.component.html',
+  styleUrls: ['./create-snack.component.css']
+})
+export class CreateSnackComponent implements OnInit {
+
+  snackForm: FormGroup
+
+  constructor(private snackService: SnackService) { }
+
+  ngOnInit() {
+    this.snackForm = new FormGroup({
+      'name': new FormControl(null),
+      'price': new FormControl(null),
+      'image': new FormControl(null)
+    })
+  }
+
+  onSubmit(){
+    this.snackService.createSnack(this.snackForm.get('name').value, this.snackForm.get('price').value, this.snackForm.get('image').value)
+    this.snackForm.reset()
+  }
+
+}
